2347f31d5b4ed1343873da0e625e9073b000615f
sam-timmins <sam.timmins1@gmail.com>
2021-09-19 11:16:43 +0000

    Add comment to delay_print in run.py

--- run.py
+++ run.py
@@ -7,5 +7,15 @@ import time
 
 
 def delay_print(text, delay):
+    """
+    Delays the print statment
+
+    Parameters:
+        text (string) - text to be printed out
+        delay (integer) - the number of seconds to delay by 
+    """
     print(text)
     time.sleep(delay)
+
+
+
