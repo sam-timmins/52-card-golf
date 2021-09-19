3c0cb66bc2d0c6018b0a9479f1811c8d879111a7
sam-timmins <sam.timmins1@gmail.com>
2021-09-19 12:32:38 +0000

    Add contne to pre_game_username_and_rules() in run.py

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def delay_print(text, delay):
 
     Parameters:
         text (string) - text to be printed out
-        delay (integer) - the number of seconds to delay by 
+        delay (integer) - the number of seconds to delay by
     """
     print(text)
     time.sleep(delay)
@@ -53,9 +53,53 @@ def format_card_to_int():
 
 def pre_game_username_and_rules():
     """
-    Makes sure the user enters a value into the user name input, then asks for the user to decide if they want to read the rules or not. If they do,
+    Makes sure the user enters a value into the user name input, then asks for
+    the user to decide if they want to read the rules or not. If they do,
     it brings up the rules, if not then starts the game
     """
+    global user_name
+    user_name = input('Please enter your name: ')
+    print()
+    while (len(user_name)) == 0:
+        user_name = input('Please enter your name: ')
+    read_rules = input(f'Hi {user_name}, would you like to read the rules of '
+                       'the game?\nyes or no: ')
+    if read_rules.lower() == 'y' or read_rules.lower() == 'yes':
+        print()
+        delay_print("A game of skill and pure luck!!", 3)
+        delay_print("The winner will have the lowest cumulative score", 1)
+        delay_print("at the end of the game.", 3)
+        delay_print("We use a standard 52-card pack.", 3)
+        delay_print("You will be asked how many cards you would like to "
+                    "play.", 3)
+        delay_print("4 cards selected will play 3 rounds", 3)
+        delay_print("6 cards selected will play 5 rounds", 3)
+        delay_print("8 cards selected will play 7 rounds", 3)
+        delay_print("The computer will randomly deal.", 3)
+        delay_print("Your cards will be shown to you at the start.", 3)
+        delay_print("They will be displayed as ['D9', 'S9', 'C2', 'H1']", 3)
+        delay_print("The first letter describes the suit...", 3)
+        delay_print("H - Hearts, D - Diamonds, S - Spades and C - Clubs", 3)
+        delay_print("The number then is the value...", 3)
+        delay_print("Ace is 1, Jack is 11, Queen is 12 and King is 13", 4)
+        delay_print("The computer will pick the top card from the deck", 1)
+        delay_print("and show it to you", 3)
+        delay_print("You then get the option to 'swap' or 'pass'", 3)
+        delay_print("If it is lower than one that you have you can 'swap'", 3)
+        delay_print("it with one of yours", 3)
+        delay_print("Choosing 'pass' ends your turn and gives the computer a "
+                    "go", 3)
+        print()
+        delay_print("Pick wisely, one card cannot be changed!!", 3)
+        print()
+        delay_print("", 3)
+        print("cards")
+    elif read_rules.lower() == 'n' or read_rules.lower() == 'no':
+        print()
+        print("cards")
+    else:
+        delay_print("Please enter a correct option...", 2)
+        user_name = input('Please enter your name: ')
 
 
 def select_number_of_cards_for_game():
@@ -112,11 +156,11 @@ def computer_game_total():
 
 def end_game():
     """
-    Gives final feedback of the game depending on the score 
+    Gives final feedback of the game depending on the score
     """
 
 
-def main();
+def main():
     """
     The main game loop
     """
