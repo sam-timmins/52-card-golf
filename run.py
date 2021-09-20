905cefe71381a55c460fac994ea0053c6b2fc0cd
sam-timmins <sam.timmins1@gmail.com>
2021-09-20 10:01:22 +0000

    Add feedback text on cards to if statement == 8 in swap_users_card() in run.py

--- run.py
+++ run.py
@@ -270,27 +270,43 @@ def swap_users_card():
     elif number_of_cards == 8:
         if formatted_swap_card_number == 1:
             user_playing_cards[0] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[0]} for {card_selection}", 2)
             display_user_cards[0] = card_selection
         elif formatted_swap_card_number == 2:
             user_playing_cards[1] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[1]} for {card_selection}", 2)
             display_user_cards[1] = card_selection
         elif formatted_swap_card_number == 3:
             user_playing_cards[2] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[2]} for {card_selection}", 2)
             display_user_cards[2] = card_selection
         elif formatted_swap_card_number == 4:
             user_playing_cards[3] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[3]} for {card_selection}", 2)
             display_user_cards[3] = card_selection
         elif formatted_swap_card_number == 5:
             user_playing_cards[4] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[4]} for {card_selection}", 2)
             display_user_cards[4] = card_selection
         elif formatted_swap_card_number == 6:
             user_playing_cards[5] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[5]} for {card_selection}", 2)
             display_user_cards[5] = card_selection
         elif formatted_swap_card_number == 7:
             user_playing_cards[6] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[6]} for {card_selection}", 2)
             display_user_cards[6] = card_selection
         elif formatted_swap_card_number == 8:
             user_playing_cards[7] = formatted_card
+            print()
+            delay_print(f"Swapping {display_user_cards[7]} for {card_selection}", 2)
             display_user_cards[7] = card_selection
         elif formatted_card != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8:
             print("Please enter either a number between 1 and 8")
