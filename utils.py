f430cb277a5825e05c7818a264d893e261351455
sam-timmins <sam.timmins1@gmail.com>
2021-09-20 21:12:44 +0000

    Add style.css file and link to layout.html

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import time
 
+
 def delay_print(text, delay):
     """
     Delays the print statement
-
     Parameters:
         text (string) - text to be printed out
         delay (integer) - the number of seconds to delay by
