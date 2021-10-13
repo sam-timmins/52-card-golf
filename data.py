4cb199367b66af9471874743304e1f8144e8e2dd
sam-timmins <sam.timmins1@gmail.com>
2021-10-13 10:03:41 +0000

    Add game_over_logo() to data.py

--- data.py
+++ data.py
@@ -94,6 +94,25 @@ def computers_move_text():
     delay_print("The computer has completed it's move..\n", 2)
 
 
+def game_over_logo():
+    """
+    Game over logo
+    """
+    print("""
+
+    
+           _____                         ____
+          / ____|                       / __ \ 
+         | |  __  __ _ _ __ ___   ___  | |  | |_   _____ _ __
+         | | |_ |/ _` | '_ ` _ \ / _ \ | |  | \ \ / / _ \ '__|
+         | |__| | (_| | | | | | |  __/ | |__| |\ V /  __/ |
+          \_____|\__,_|_| |_| |_|\___|  \____/  \_/ \___|_|
+
+
+          
+        """)
+
+
 def game_over_text():
     """
     Text for game over
@@ -108,7 +127,7 @@ def game_over_text():
 
 
           
-          """)
+    """)
     delay_print("Totaling the scores...", 3)
     delay_print("Ready in...", 2)
     delay_print("3...", 1)
