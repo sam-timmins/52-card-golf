55cb375ee7d2ad7cda5ec5c61839fac8aa3c398a
sam-timmins <sam.timmins1@gmail.com>
2021-10-13 13:44:35 +0000

    Add import of game_button from data.py

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ from utils import delay_print, clear_terminal
 from data import user_playing_cards, computer_playing_cards, \
     display_user_cards, the_deck, game_rules, logo, game_over_text, \
     select_number_of_cards_for_game_text, SHEET, \
-    computer_jokes, computers_move_logo, game_over_logo
+    computer_jokes, computers_move_logo, game_over_logo, game_button
 
 
 def pick_card_from_deck():
